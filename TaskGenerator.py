28bccf20f83958b7cb7029c0dda946c5ebfa801e
evgen <evgen19-97@ya.ru>
2018-06-19 19:31:24 +0300

    Now we can add different type of cells and add a query

--- TaskGenerator.py
+++ TaskGenerator.py
@@ -28,17 +28,29 @@ def CreateTask(week_number=1, student_with_variants_path='StudentsWithVariants.x
                 with open(task_path.format(cell[2]), 'r', encoding="utf8") as json_file:
                     data_task = json.load(json_file)
                 data = data_task[str(student[1]['Weak 1 Task 1 '])]
-                cells_source.append([data, 'm'])  ## Не обязательно код!
+
+                for data_cell in data:
+                    cells_source.append([data_cell[0], data_cell[1]])
+
             elif cell[0] == 'Head':
                 with open(head_path, 'r', encoding="utf8") as json_file:
                     data_task = json.load(json_file)
-                data = data_task['text']
-                cells_source.append([data, 'm'])
+                #data = data_task['text']
+                #cells_source.append([data, 'm'])
+
+                for data_cell in data_task:
+                    # Первое текст, второе тип - мы идем по ключам словаря. format - чтобы добавить имя
+                    cells_source.append([data_task[data_cell][0].format(student[1]['Student']), data_task[data_cell][1]])
+
+
             elif cell[0] == 'Th':
                 with open(theory_path, 'r', encoding="utf8") as json_file:
                     data_task = json.load(json_file)
-                data = data_task[cell[2]]
-                cells_source.append([data, 'm'])
+                data = data_task[cell[2]] # Получили номер необходимой нам теории
+                #print(data)
+                for data_cell in data:
+                    cells_source.append([data_cell[0], data_cell[1]])
+                #cells_source.append([data, 'm'])
 
         cells_to_write = NotebookGenerator.cells_query(cells_source)
         student_path = output_path + '/{0}_week{1}.ipynb'.format(student[1]['Student'], week_number)
