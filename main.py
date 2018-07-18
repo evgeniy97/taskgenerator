d5d551acaea1c6b1f80a96470a2b10aa28348561
evgen <evgen19-97@ya.ru>
2018-07-18 12:21:51 +0300

    Rewrite scriptwriter as class

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import ScriptWriter
 
 if __name__ == '__main__':
     VariantGenerator.GenerateVariantsDistribution(random_seed_parametr=0)
-    ScriptWriter.scriptwriter()
+    ScriptWriter.scriptwrite().write()
 
 
