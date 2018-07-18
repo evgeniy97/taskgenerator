a7fd0643577f0281c959f8bd1974990a6c85f10c
evgen <evgen19-97@ya.ru>
2018-07-18 12:32:54 +0300

    Rewrite VariantGenerator as class

--- VariantGenerator.py
+++ VariantGenerator.py
@@ -2,111 +2,116 @@ import numpy as np
 import pandas as pd
 import os
 
-def __create_ALL_LR(COURSE_PATH = 'BaseNotebooks'):
 
-    path = os.path.join(os.getcwd(), COURSE_PATH)
-    tree = os.walk(path) # tree is a generator, so us next
-    files = next(tree)
-    py_list = [j for j in files[2] if j[-3:] == '.py'] # get only python files
+class variantgenerator():
+    def __init__(self, seed = 0):
+        self.COURSE_PATH = 'BaseNotebooks'
+        self.random_seed_parametr = seed
+        self.student_path = 'students.xlsx'
+        self.students_with_variants_path = 'StudentsWithVariants.xlsx'
 
-    with open(path + '\All_LR.py', 'w', encoding="utf8") as output_file:
+    def __create_ALL_LR(self):
 
-        source = ''
-        for i in py_list:
-            source += ("import {0}.{1} as {1}".format(COURSE_PATH, i[:-3]) + '\n')
-        source += 'Tasks_db = ['
-        for i in py_list:
+        path = os.path.join(os.getcwd(), self.COURSE_PATH)
+        tree = os.walk(path) # tree is a generator, so us next
+        files = next(tree)
+        py_list = [j for j in files[2] if j[-3:] == '.py'] # get only python files
 
-            source += ("{0}.Tasks_db".format(i[:-3]) + '\n')
-            source += ']'
-        output_file.write(source)
-    print('Create ' + path + '\All_LR.py')
+        with open(path + '\All_LR.py', 'w', encoding="utf8") as output_file:
 
-def __generate_stracture(COURSE_PATH = 'BaseNotebooks'):
-    from BaseNotebooks.All_LR import Tasks_db
-    tasks_num = []
-    var_num = []
+            source = ''
+            for i in py_list:
+                source += ("import {0}.{1} as {1}".format(self.COURSE_PATH, i[:-3]) + '\n')
+            source += 'Tasks_db = ['
+            for i in py_list:
 
-    for week in Tasks_db:
-        tasks_num.append(len(week))
-        for task_key in week.keys():
-            var_num.append((len(week[task_key])))
+                source += ("{0}.Tasks_db".format(i[:-3]) + '\n')
+                source += ']'
+            output_file.write(source)
+        print('Create ' + path + '\All_LR.py')
 
-    print('DELETE ' + os.path.join(os.getcwd(), COURSE_PATH) + '\ALL_LR.py')
-    os.remove(os.path.join(os.getcwd(), COURSE_PATH) + '\ALL_LR.py')
+    def __generate_stracture(self):
+        from BaseNotebooks.All_LR import Tasks_db
+        tasks_num = []
+        var_num = []
 
-    return  tasks_num, var_num
+        for week in Tasks_db:
+            tasks_num.append(len(week))
+            for task_key in week.keys():
+                var_num.append((len(week[task_key])))
 
-def __GenerateVariantsDistribution(random_seed_parametr = 0,student_path = 'students.xlsx'
-                                   , students_with_variants_path='StudentsWithVariants.xlsx'):
-    """
-    Generate variant distribution with random seed
-    :param random_seed_parametr: parametr to np.random.seed
-    :param student_path: path to students list
-    :return: none
-    """
-    np.random.seed(random_seed_parametr)
-    try:
-        Students = pd.read_excel(student_path)
-        students_number = len(Students)
+        print('DELETE ' + os.path.join(os.getcwd(), self.COURSE_PATH) + '\ALL_LR.py')
+        os.remove(os.path.join(os.getcwd(), self.COURSE_PATH) + '\ALL_LR.py')
 
-        __create_ALL_LR()
-        Course_structure, variants_numbers = __generate_stracture()
+        return  tasks_num, var_num
 
-        Number_of_weaks = len(Course_structure)
+    def __GenerateVariantsDistribution(self):
+        """
+        Generate variant distribution with random seed
+        :param random_seed_parametr: parametr to np.random.seed
+        :param student_path: path to students list
+        :return: none
+        """
+        np.random.seed(self.random_seed_parametr)
+        try:
+            Students = pd.read_excel(self.student_path)
+            students_number = len(Students)
 
-        number_of_distribution = 0
-        for WeakNumber in range(Number_of_weaks):
-            for TaskNumber in range(Course_structure[WeakNumber]):
-                Students['Week {0} Task {1}'.format(WeakNumber + 1, TaskNumber + 1)] = np.random.randint(
-                    variants_numbers[number_of_distribution], size=students_number)
-                number_of_distribution += 1
+            self.__create_ALL_LR()
+            Course_structure, variants_numbers = self.__generate_stracture()
 
-        writer = pd.ExcelWriter(students_with_variants_path)
-        Students.to_excel(writer)
-        writer.save()
-    except:
-        print('File with students doesnot exist')
+            Number_of_weaks = len(Course_structure)
 
-def Check_New_Students(student_path='students.xlsx', students_with_variants_path='StudentsWithVariants.xlsx'):
-    try:
-        Students = pd.read_excel(student_path)
-    except:
-        print('File with students doesnot exist')
-        return None
-    try:
-        Students_with_variants = pd.read_excel(students_with_variants_path)
-    except:
-        print('No Students_with_variants file')
-        return True
-    if len(Students) != len(Students_with_variants):
-        return True
-    else:
-        return False
+            number_of_distribution = 0
+            for WeakNumber in range(Number_of_weaks):
+                for TaskNumber in range(Course_structure[WeakNumber]):
+                    Students['Week {0} Task {1}'.format(WeakNumber + 1, TaskNumber + 1)] = np.random.randint(
+                        variants_numbers[number_of_distribution], size=students_number)
+                    number_of_distribution += 1
 
-def SortByName(students_with_variants_path='StudentsWithVariants.xlsx'):
-    try:
-        Students_with_variants = pd.read_excel(students_with_variants_path)
-        Students_with_variants = Students_with_variants.sort_values(by='Student')
-        writer = pd.ExcelWriter(students_with_variants_path)
-        Students_with_variants.to_excel(writer, index=False)
-        writer.save()
-    except:
-        print('No Students_with_variants file')
-        return None
+            writer = pd.ExcelWriter(self.students_with_variants_path)
+            Students.to_excel(writer)
+            writer.save()
+        except:
+            print('File with students doesnot exist')
 
-def GenerateVariantsDistribution(random_seed_parametr=0, student_path='students.xlsx',
-                                 students_with_variants_path='StudentsWithVariants.xlsx' ):
-    """
-    generate variant distrbution, and sorted it
-    :param random_seed_parametr: parametr to np.random.seed
-    :param student_path: path to students list
-    :param students_with_variants_path: path to students list with variants
-    """
-    if Check_New_Students(student_path, students_with_variants_path):
-        __GenerateVariantsDistribution(random_seed_parametr,student_path, students_with_variants_path)
-        SortByName()
+    def Check_New_Students(self):
+        try:
+            Students = pd.read_excel(self.student_path)
+        except:
+            print('File with students doesnot exist')
+            return None
+        try:
+            Students_with_variants = pd.read_excel(self.students_with_variants_path)
+        except:
+            print('No Students_with_variants file')
+            return True
+        if len(Students) != len(Students_with_variants):
+            return True
+        else:
+            return False
+
+    def SortByName(self):
+        try:
+            Students_with_variants = pd.read_excel(self.students_with_variants_path)
+            Students_with_variants = Students_with_variants.sort_values(by='Student')
+            writer = pd.ExcelWriter(self.students_with_variants_path)
+            Students_with_variants.to_excel(writer, index=False)
+            writer.save()
+        except:
+            print('No Students_with_variants file')
+            return None
+
+    def GenerateVariantsDistribution(self):
+        """
+        generate variant distrbution, and sorted it
+        :param random_seed_parametr: parametr to np.random.seed
+        :param student_path: path to students list
+        :param students_with_variants_path: path to students list with variants
+        """
+        if self.Check_New_Students():
+            self.__GenerateVariantsDistribution()
+            self.SortByName()
 if __name__ == '__main__':
-    #__generate_stracture()
-    GenerateVariantsDistribution()
+    variantgenerator().GenerateVariantsDistribution()
 
